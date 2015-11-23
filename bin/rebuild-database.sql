019af73355fd2b83c8a0990641f1816d6eb94fb0
KhanhTran <quockhanhqb011@gmail.com>
2015-11-23 23:39:14 +0700

    Create init database for ruoi

--- bin/rebuild-database.sql
+++ bin/rebuild-database.sql
@@ -1,4 +1,93 @@
 Phan tich database theo Thong tin website http://timcanho.com.vn/ cho Ruoi
 
 
-CREATE TABLE Product
+
+CREATE TABLE UsersGroup (
+  userGroupId BIGINT(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  code VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  description VARCHAR(255),
+  UNIQUE (code)
+);
+
+CREATE TABLE Users (
+  userId BIGINT(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  userGroupId BIGINT(20) DEFAULT NULL,
+  username varchar(255) DEFAULT NULL,
+  password varchar(255) DEFAULT NULL,
+  email varchar(255) DEFAULT NULL,
+  firstName varchar(255) DEFAULT NULL,
+  lastName varchar(255) DEFAULT NULL,
+  displayName varchar(255) DEFAULT NULL,
+  phoneNumber varchar(255) DEFAULT NULL,
+  status int(11) DEFAULT NULL,
+  createdDate datetime DEFAULT NULL,
+  modifiedDate datetime DEFAULT NULL,
+  avatar varchar(255) DEFAULT NULL,
+  fullAccess int(11) DEFAULT NULL,
+  PRIMARY KEY (userID),
+  UNIQUE (username),
+  FOREIGN KEY (userGroupId) REFERENCES UsersGroup(userGroupId)
+);
+
+CREATE TABLE ProductType (
+  productTypeId BIGINT(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  code VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  displayOrder INT NOT NULL DEFAULT 0,
+  UNIQUE (code)
+);
+
+CREATE TABLE ProductCategory (
+  productCategoryId BIGINT(20) PRIMARY KEY,
+  code VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  displayOrder INT NOT NULL DEFAULT 0,
+  parentId BIGINT(20),
+  UNIQUE (code),
+  FOREIGN KEY (parentId) REFERENCES ProductCategory(productCategoryId)
+);
+
+CREATE TABLE Product (
+  productId BIGINT(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  productTypeId BIGINT(20) NOT NULL,
+  productCategoryId BIGINT(20) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  keyword VARCHAR(255) NOT NULL,
+  prefixUrl VARCHAR(255),
+  shortDescription VARCHAR(255) NOT NULL,
+  description TEXT,
+  thumbnails VARCHAR(255),
+  displayOrder INT NOT NULL DEFAULT 0,
+  createdDate TIMESTAMP NOT NULL,
+  modifiedDate TIMESTAMP,
+  publishedDate TIMESTAMP,
+  author VARCHAR(100),
+  UNIQUE (title),
+  FOREIGN KEY (productTypeId) REFERENCES ProductType(productTypeId),
+  FOREIGN KEY (productCategoryId) REFERENCES ProductCategory(productCategoryId)
+);
+
+CREATE TABLE ProductComment (
+  productCommentId BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  productId BIGINT(20) NOT NUll,
+  customerName VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  phoneNumber VARCHAR(20),
+  message TEXT NOT NULL,
+  createdDate TIMESTAMP NOT NULL,
+  resolvedDate TIMESTAMP,
+  resolvedBy VARCHAR(255)
+);
+
+CREATE TABLE Customer (
+   productCommentId BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+   customerName VARCHAR(255) NOT NULL,
+   email VARCHAR(255) NOT NULL,
+   phoneNumber VARCHAR(20),
+   location VARCHAR(255),
+   autoGetEmail int(11) DEFAULT 0,
+   createdDate TIMESTAMP NOT NULL,
+)
+
+
